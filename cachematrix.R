3d98679b281adf785cc4e80fae937c2ca3e55ebc
shy-runge <r.shyamsunder@gmail.com>
2015-03-21 18:50:17 +0800

    assignment 2 cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a special "matrix", 
+#which is really a list containing a function to
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        minv <- NULL
+        set <- function(y) {
+                x <<- y
+                minv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) minv <<- solve
+        getinverse <- function() minv
+        list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
 
@@ -12,4 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        minv <- x$getinverse()
+        if(!is.null(minv)) {
+                message("getting cached data")
+                return(minv)
+        }
+        data <- x$get()
+        minv <- solve(data, ...)
+        x$setinverse(minv)
+        minv
+        
 }
